5f96736ab7fa9bd7ccf509aa509e5b0e284f4d6b
Karno <karnoroid@gmail.com>
2019-10-11 12:42:04 +0900

    Hooray! The menu have been successfully displayed!

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+interface SushiInfo {
+  category: string;
+  name: string;
+  price: number;
+  energy: number;
+  link: string;
+  id: number;
+  cat_id: number;
+}
+
+interface SushiHandler {
+  sushi: SushiInfo;
+}
+
+const Sushi: React.FC<SushiHandler> = (prop) => {
+  return (
+    <div className="sushi">
+      <h3><a href={prop.sushi.link}>{prop.sushi.category} - {prop.sushi.name}</a> </h3>
+      <p>{prop.sushi.price} yen, {prop.sushi.energy} kcal</p>
+    </div>
+  )
+}
+
 const App: React.FC = () => {
+  const [menu, setMenu] = useState<SushiInfo[] | undefined>(undefined);
+
+  useEffect(() => {
+    // fetch menu
+    (async () => {
+      const resp = await fetch("sushiro_menu.json");
+      let json = await resp.json();
+      const sushies = Object.keys(json)
+        .map(c => [c, json[c]])
+        .flatMap(t => t[1].map((item: any) => {
+          return {
+            category: t[0],
+            name: item.name,
+            price: item.price,
+            energy: item.energy,
+            link: item.link,
+            id: item.id,
+            cat_id: item.cat
+          }
+        }));
+
+      setMenu(sushies);
+    })();
+  });
+
+  if (menu == undefined) {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>loading...</p>
+        </header>
+      </div>
+    )
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {menu.map(s => <Sushi sushi={s} />)}
       </header>
     </div>
   );
